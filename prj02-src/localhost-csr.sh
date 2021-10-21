86382344afe5fcb6795a047eeaee8ad9235e8e56
alcegarza <rocioacgz@hotmail.com>
2021-10-20 21:33:18 -0500

    Attack

--- prj02-src/localhost-csr.sh
+++ prj02-src/localhost-csr.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 openssl req -config localhost.cnf -newkey rsa:2048 -sha256 \
-  -keyout ece443-localhost.key -out ece443-localhost.csr \
+  -keyout ece443-hacked.key -out ece443-hacked.csr \
   -nodes -batch
 
--- prj02-src/sign-localhost.sh
+++ prj02-src/sign-localhost.sh
@@ -2,6 +2,6 @@
 
 openssl ca -cert ece443-CA.pem -keyfile ece443-CA.key \
   -config ca.cnf -policy signing_policy -extensions signing_req \
-  -outdir . -out ece443-localhost.pem -in ece443-localhost.csr \
+  -outdir . -out ece443-hacked.pem -in ece443-hacked.csr \
   -batch
 
